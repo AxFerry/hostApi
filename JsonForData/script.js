86ce3bc319686f5c92a265122524673f9c41c957
AxFerry <162710384+AxFerry@users.noreply.github.com>
2024-03-16 15:23:07 +0100

    Add files via upload

---
+++ JsonForData/script.js
@@ -0,0 +1,20 @@
+let formt = document.querySelector('form');
+let formd = new FormData(formt);
+formd.addEventListener('submit' , handleSubmit())  
+
+
+function handleSubmit(event){
+    
+    let dati = Object.fromEntries(formd) ;
+    let JsonDati = JSON.stringify(dati) ;
+    event.preventDefault();
+    fetch('https://github.com/AxFerry/hostApi/data.json',{
+         method: 'POST' ,
+         headers: {
+            'Content-Type' : 'application/json'
+         },
+         body : JsonDati
+        }).then(res => res.json())
+        .then(result => alert(result.data))
+        .catch(err => alert(err))
+}
